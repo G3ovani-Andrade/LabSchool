fada8d21049fb3d2850de82b42e6d809cc17106f
gabiAlmeidaDev <153233088+gabiAlmeidaDev@users.noreply.github.com>
2024-02-19 19:31:17 -0300

    Ajuste chaves main

--- src/Main.java
+++ src/Main.java
@@ -93,172 +93,171 @@ public class Main {
                                                 }
                                             }
                                             break;
+                                        case 2:
+                                            //menuRemover() - Su
+                                            //remover professor
+                                            //remover aluno
+                                            //remover curso
+                                            //remover turma
+                                            //remover diretor
+                                            break;
+                                        case 3:
+                                            System.out.print("""
+
+                                                    Menu Listar
+                                                    -----------------------------
+                                                    1 - Listar Professores
+                                                    2 - Listar Diretores
+                                                    3 - Listar Alunos
+                                                    4 - Listar Cursos
+                                                    5 - Listar Turmas
+                                                    Escolha a operação desejada:\s""");
+                                            switch (ScannerUtil.nextInt()) {
+                                                case 1:
+                                                    dadosProfessores.listarProfessor();
+                                                    break;
+                                                case 2:
+                                                    dadosDiretor.listarDiretor();
+                                                    break;
+                                                case 3:
+                                                    dadosAlunos.listarAluno();
+                                                    break;
+                                                case 4:
+                                                    dadosCursos.listarCursos();
+                                                    break;
+                                                case 5:
+                                                    dadosTurmas.listarTurmas();
+                                                    break;
+                                                default:
+                                                    System.out.println("Opção invalida");
+                                            }
+                                            break;
+
+                                        case 4:
+                                            dadosDiretor.getDiretores().get(0).promoverProfessor(dadosProfessores);
+                                        case 0:
+                                            break LOOPMENUDIRETOR;
                                     }
-                                    case 2:
-                                        //menuRemover() - Su
-                                        //remover professor
-                                        //remover aluno
-                                        //remover curso
-                                        //remover turma
-                                        //remover diretor
-                                        break;
-                                    case 3:
-                                        System.out.print("""
+                                }
+                            }
+                        } else if (tipoFuncionario == 2) {
+                            boolean retornoLogin = dadosProfessores.loginProfessor();
+                            if (retornoLogin) {
+                                LOOPMENUPROFESSOR:
+                                while (true) {
+                                    System.out.print("""
 
-                                                Menu Listar
-                                                -----------------------------
-                                                1 - Listar Professores
-                                                2 - Listar Diretores
-                                                3 - Listar Alunos
-                                                4 - Listar Cursos
-                                                5 - Listar Turmas
-                                                Escolha a operação desejada:\s""");
-                                        switch (ScannerUtil.nextInt()) {
-                                            case 1:
-                                                dadosProfessores.listarProfessor();
-                                                break;
-                                            case 2:
-                                                dadosDiretor.listarDiretor();
-                                                break;
-                                            case 3:
-                                                dadosAlunos.listarAluno();
-                                                break;
-                                            case 4:
-                                                dadosCursos.listarCursos();
-                                                break;
-                                            case 5:
-                                                dadosTurmas.listarTurmas();
-                                                break;
-                                            default:
-                                                System.out.println("Opção invalida");
-                                        }
-                                        break;
-
-                                    case 4:
-                                        dadosDiretor.getDiretores().get(0).promoverProfessor(dadosProfessores);
-                                    case 0:
-                                        break LOOPMENUDIRETOR;
+                                            Menu Professor
+                                            -----------------------------
+                                            1 - Adicionar Aluno a Turma
+                                            2 - Remover Aluno da Turma
+                                            3 - Listar Alunos
+                                            0 - Voltar Menu Inicial
+                                            Escolha a operação desejada:\s""");
+                                    switch (scanner.nextInt()) {
+                                        case 1:
+                                            dadosAlunos.criarAluno();
+                                            break;
+                                        case 2:
+                                            //remover aluno turma - Su
+                                            break;
+                                        case 3:
+                                            dadosAlunos.listarAluno();
+                                            break;
+                                        case 0:
+                                            break LOOPMENUPROFESSOR;
+                                    }
                                 }
                             }
                         }
-                } else if (tipoFuncionario == 2) {
-                boolean retornoLogin = dadosProfessores.loginProfessor();
-                if (retornoLogin) {
-                    LOOPMENUPROFESSOR:
-                    while (true) {
+                        break;
+                    case 2:
                         System.out.print("""
 
-                                Menu Professor
+                                Menu Tipo Funcionário
                                 -----------------------------
-                                1 - Adicionar Aluno a Turma
-                                2 - Remover Aluno da Turma
-                                3 - Listar Alunos
+                                1 - Diretor
+                                2 - Professor
                                 0 - Voltar Menu Inicial
-                                Escolha a operação desejada:\s""");
-                        switch (scanner.nextInt()) {
-                            case 1:
-                                dadosAlunos.criarAluno();
-                                break;
-                            case 2:
-                                //remover aluno turma - Su
-                                break;
-                            case 3:
-                                dadosAlunos.listarAluno();
-                                break;
-                            case 0:
-                                break LOOPMENUPROFESSOR;
+                                Escolha o tipo funcionário:\s""");
+                        int tipoFuncionarioCadastro = scanner.nextInt();
+                        if (tipoFuncionarioCadastro == 1) {
+                            dadosDiretor.criarDiretor();
+                        } else if (tipoFuncionarioCadastro == 2) {
+                            dadosProfessores.criarProfessor();
                         }
-                    }
+                        break;
+                    case 0:
+                        System.out.println("Voltando ao menu inicial.");
+                        break;
+                    default:
+                        System.out.println("\u001B[31m" + "Opção inválida!" + "\u001B[0m");
+                        break;
                 }
-            }
                 break;
             case 2:
                 System.out.print("""
 
-                        Menu Tipo Funcionário
+                        Menu Entrada
                         -----------------------------
-                        1 - Diretor
-                        2 - Professor
+                        1 - Login
+                        2 - Cadastro
                         0 - Voltar Menu Inicial
-                        Escolha o tipo funcionário:\s""");
-                int tipoFuncionarioCadastro = scanner.nextInt();
-                if (tipoFuncionarioCadastro == 1) {
-                    dadosDiretor.criarDiretor();
-                } else if (tipoFuncionarioCadastro == 2) {
-                    dadosProfessores.criarProfessor();
-                }
-                break;
-            case 0:
-                System.out.println("Voltando ao menu inicial.");
-                break;
-            default:
-                System.out.println("\u001B[31m" + "Opção inválida!" + "\u001B[0m");
-                break;
-        }
-        break;
-        case 2:
-        System.out.print("""
+                        Escolha opção desejado:\s""");
+                switch (scanner.nextInt()) {
+                    case 1:
+                        Aluno alunoLogado = dadosAlunos.loginAluno();
+                        if (alunoLogado.getNome().isEmpty()) {
+                            LOOPMENUALUNO:
+                            while (true) {
+                                System.out.print("""
 
-                Menu Entrada
-                -----------------------------
-                1 - Login
-                2 - Cadastro
-                0 - Voltar Menu Inicial
-                Escolha opção desejado:\s""");
-        switch (scanner.nextInt()) {
-            case 1:
-                Aluno alunoLogado = dadosAlunos.loginAluno();
-                if (alunoLogado.getNome().isEmpty()) {
-                    LOOPMENUALUNO:
-                    while (true) {
-                        System.out.print("""
-
-                                Menu Aluno
-                                -----------------------------
-                                1 - Listar Cursos
-                                2 - Ativar Matrícula
-                                3 - Trancar Matrícula
-                                4 - Entrar Turma
-                                0 - Voltar Menu Inicial
-                                Escolha a operação desejada:\s""");
-                        switch (scanner.nextInt()) {
-                            case 1:
-                                dadosCursos.listarCursos();
-                                break;
+                                        Menu Aluno
+                                        -----------------------------
+                                        1 - Listar Cursos
+                                        2 - Ativar Matrícula
+                                        3 - Trancar Matrícula
+                                        4 - Entrar Turma
+                                        0 - Voltar Menu Inicial
+                                        Escolha a operação desejada:\s""");
+                                switch (scanner.nextInt()) {
+                                    case 1:
+                                        dadosCursos.listarCursos();
+                                        break;
+                                    case 2:
+                                        //ativar matricula - Geovani
+                                        break;
+                                    case 3:
+                                        //trancar matricula - Geovani
+                                        break;
+                                    case 4:
+                                        dadosCursos.escolherCurso(alunoLogado);
+                                        break;
+                                    case 0:
+                                        break LOOPMENUALUNO;
+                                }
+                            }
+                            break;
                             case 2:
-                                //ativar matricula - Geovani
-                                break;
-                            case 3:
-                                //trancar matricula - Geovani
-                                break;
-                            case 4:
-                                dadosCursos.escolherCurso(alunoLogado);
+                                dadosAlunos.criarAluno();
                                 break;
                             case 0:
-                                break LOOPMENUALUNO;
+                                System.out.println("Voltando ao menu inicial.");
+                                break;
+                            default:
+                                System.out.println("\u001B[31m" + "Opção inválida!" + "\u001B[0m");
+                                break;
                         }
-                    }
+                        break;
+                    case 0:
+                        System.out.println("Sistema encerrado.");
+                        break;
+                    default:
+                        System.out.println("\u001B[31m" + "Opção inválida!" + "\u001B[0m");
+                        break;
                 }
-                break;
-            case 2:
-                dadosAlunos.criarAluno();
-                break;
-            case 0:
-                System.out.println("Voltando ao menu inicial.");
-                break;
-            default:
-                System.out.println("\u001B[31m" + "Opção inválida!" + "\u001B[0m");
-                break;
         }
-        break;
-        case 0:
-        System.out.println("Sistema encerrado.");
-        break;
-        default:
-        System.out.println("\u001B[31m" + "Opção inválida!" + "\u001B[0m");
-        break;
     }
-}
 }
 
 //Para testes:
